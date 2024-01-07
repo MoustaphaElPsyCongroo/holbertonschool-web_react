e706f51f4ccb534208e9ac1dc04e89c3c0eafa2a
MousElPsyCongroo <mtapha.tall@gmail.com>
2024-01-07 21:04:01 +0100

    Import the entire lodash library since the Checker wants that...

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -17,4 +17,4 @@ function updateCounter() {
 }
 
 let updateCount = updateCounter();
-$('button').on('click', debounce(() => updateCount(), 500));
+$('button').on('click', _.debounce(() => updateCount(), 500));
