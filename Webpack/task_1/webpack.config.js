d9d185741c8c1a6c59cd6e12c52a50c02b524384
MousElPsyCongroo <mtapha.tall@gmail.com>
2024-01-07 21:00:30 +0100

    Remove html-webpack-plugin for the Checker

--- Webpack/task_1/webpack.config.js
+++ Webpack/task_1/webpack.config.js
@@ -1,12 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack task 1'
-    })
-  ],
   entry: {
     main: path.resolve(__dirname, './js/dashboard_main.js')
   },
