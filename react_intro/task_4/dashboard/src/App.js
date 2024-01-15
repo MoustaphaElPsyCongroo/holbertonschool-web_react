88c11f674d871798d7f2a2bc92eb87c904d78afe
MousElPsyCongroo <mtapha.tall@gmail.com>
2024-01-15 23:52:34 +0100

    Configure gh-pages deployment

---
+++ react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" />
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" />
+          <button type="submit">OK</button>
+        </form>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
