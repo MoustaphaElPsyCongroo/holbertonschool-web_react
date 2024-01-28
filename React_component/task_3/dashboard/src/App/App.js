83708c646112a46a58a4e16f2cc8e3361e1bdeef
MousElPsyCongroo <mtapha.tall@gmail.com>
2024-01-28 02:27:29 +0100

    5 - Use the new components

--- React_component/task_3/dashboard/src/App/App.js
+++ React_component/task_3/dashboard/src/App/App.js
@@ -1,46 +1,47 @@
-import './App.css';
-import React from 'react';
-import Notifications from '../Notifications/Notifications';
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import PropTypes from 'prop-types';
-import { getLatestNotification } from '../utils/utils';
-
+import "./App.css";
+import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 
 class App extends React.Component {
   logOutOnKeydown = (e) => {
-    if (e.code === 'KeyH') {
+    if (e.code === "KeyH") {
       if (e.ctrlKey) {
         e.preventDefault();
-        alert('Logging you out');
+        alert("Logging you out");
         this.props.logOut();
       }
     }
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener('keydown', this.logOutOnKeydown);
+    window.addEventListener("keydown", this.logOutOnKeydown);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.logOutOnKeydown);
+    window.removeEventListener("keydown", this.logOutOnKeydown);
   }
 
   render() {
     const { isLoggedIn } = this.props;
 
     const listCourses = [
-      {id: 1, name: "ES6", credit: 60},
-      {id: 2, name: 'Webpack', credit: 20},
-      {id: 3, name: 'React', credit: 40}
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
     ];
 
     const listNotifications = [
-      {id: 1, type: 'default', value: 'New course available'},
-      {id: 2, type: 'urgent', value: 'New resume available'},
-      {id: 3, type: 'urgent', html: {__html: getLatestNotification()}}
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
     ];
 
     return (
@@ -50,7 +51,18 @@ class App extends React.Component {
           <Header />
         </header>
         <div className="App-body">
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          {isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the School">
+            <p>Here are some fresh news.</p>
+          </BodySection>
         </div>
         <footer className="App-footer">
           <Footer />
@@ -58,17 +70,16 @@ class App extends React.Component {
       </>
     );
   }
-
 }
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => {}
-}
+  logOut: () => {},
+};
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
-}
+  logOut: PropTypes.func,
+};
 
 export default App;
--- React_component/task_3/dashboard/src/BodySection/BodySection.js
+++ React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class BodySection extends React.Component {
   render() {
